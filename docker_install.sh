6987177e462a2c29503bc5ff05fae81098b6ee1f
Yassine MILHI <milhi.yassinee@gmail.com>
2022-06-05 22:45:15 +0200

    Add autostart docker.

--- docker_install.sh
+++ docker_install.sh
@@ -11,3 +11,4 @@ echo \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
+sudo systemctl enable docker
